6fac1799386f12a007b65482dba46d2c5729a0db
Joel Angeles <angelesurrutia.j@gmail.com>
2025-01-24 14:21:39 -0600

    add inicial project

---
+++ src/main/java/com/angelesdev/SpringAPI/SpringApiApplication.java
@@ -0,0 +1,15 @@
+package com.angelesdev.SpringAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiApplication.class, args);
+
+		System.out.println("\nSERVER IS RUNNING.");
+	}
+
+}
